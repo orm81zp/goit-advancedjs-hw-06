f4341a8dd665f4bf12bf93335046eb8ce71b0f58
Roman Onishchenko <roman.onishchenko@onelog.ch>
2024-07-26 16:55:14 +0300

    update the files in src folder

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,20 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum WeekDays {
+  monday = "monday",
+  tuesday = "tuesday",
+  wednesday = "wednesday",
+  thursday = "thursday",
+  friday = "friday",
+  saturday = "saturday",
+  sunday = "sunday",
+}
+
+const WEEKEND_DAYS = [WeekDays.saturday, WeekDays.sunday];
+
+const isWeekend = (day: WeekDays) => WEEKEND_DAYS.includes(day);
+
+console.log(isWeekend(WeekDays.monday));
+console.log(isWeekend(WeekDays.saturday));
